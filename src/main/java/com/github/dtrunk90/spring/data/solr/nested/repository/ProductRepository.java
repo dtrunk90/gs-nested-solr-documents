96f96b4f8c328383e568bca316cb634d72906316
Danny Trunk <dtrunk90@gmail.com>
2017-06-30 18:45:47 +0200

    fix query fields

--- src/main/java/com/github/dtrunk90/spring/data/solr/nested/repository/ProductRepository.java
+++ src/main/java/com/github/dtrunk90/spring/data/solr/nested/repository/ProductRepository.java
@@ -9,7 +9,7 @@ import com.github.dtrunk90.spring.data.solr.nested.document.Product;
 @Repository
 public interface ProductRepository extends SolrCrudRepository<Product, String> {
 
-	@Query(fields = {"*", "child parentFilter=variant:false"})
+	@Query(fields = {"*", "[child parentFilter=variant:false]"})
 	Iterable<Product> findAll();
 
 }
